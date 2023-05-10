b01620df09eeeb5d6fb0fe049a75ed78a22afdd9
PaulinaWalasiewicz <115495742+PaulinaWalasiewicz@users.noreply.github.com>
2023-05-10 19:37:16 +0200

    Controllers

--- src/main/java/com/example/storeeverything/Repository/NoteRepository.java
+++ src/main/java/com/example/storeeverything/Repository/NoteRepository.java
@@ -1,8 +1,11 @@
 package com.example.storeeverything.Repository;
 
+import com.example.storeeverything.data.Category;
 import com.example.storeeverything.data.Note;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface NoteRepository extends MongoRepository<Note,String> {
     @Query("{'title':?0}")
@@ -10,4 +13,10 @@ public interface NoteRepository extends MongoRepository<Note,String> {
 
     public long count();
 
+    List<Note> findByUserIdOrderByCreatedAtAsc(Integer userId);
+
+    List<Note> findByUserIdOrderByCreatedAtDesc(Integer userId);
+    List<Note> findByUserIdAndAndCategory_Id(Integer userId,Integer categoryId);
+
+    List<Note> findByUserId(Integer userId);
 }
--- src/main/java/com/example/storeeverything/Repository/UserRepository.java
+++ src/main/java/com/example/storeeverything/Repository/UserRepository.java
@@ -7,6 +7,9 @@ import org.springframework.data.mongodb.repository.Query;
 public interface UserRepository extends MongoRepository<User,String> {
     @Query("{'username': ?0}")
     User findUserByUsername(String username);
-
+    @Query("{'id': ?0}")
+    User findUserById(String id);
     public long count();
+
+    User findByUsername(String username);
 }
