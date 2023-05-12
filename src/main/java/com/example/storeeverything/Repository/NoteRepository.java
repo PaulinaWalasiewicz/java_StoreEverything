69fc03bd16f80134cbeaaee54dac7e85ff026e44
PaulinaWalasiewicz <115495742+PaulinaWalasiewicz@users.noreply.github.com>
2023-05-12 09:38:37 +0200

    Notes sorted by title and category alphabetically

--- src/main/java/com/example/storeeverything/Repository/NoteRepository.java
+++ src/main/java/com/example/storeeverything/Repository/NoteRepository.java
@@ -5,6 +5,9 @@ import com.example.storeeverything.data.Note;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 public interface NoteRepository extends MongoRepository<Note,String> {
@@ -19,4 +22,15 @@ public interface NoteRepository extends MongoRepository<Note,String> {
     List<Note> findByUserIdAndAndCategory_Id(Integer userId,Integer categoryId);
 
     List<Note> findByUserId(Integer userId);
+
+    List<Note> findByUserOrderByTitleDesc(Integer userId);
+    List<Note> findByUserOrderByTitleAsc(Integer userId);
+
+
+    List<Note> findByUserOrderByCategoryDesc(Integer userId);
+    List<Note> findByUserOrderByCategoryAsc(Integer userId);
+
+//    List<Note> findByCreatedAtDate(LocalDateTime createdAt_date);
+
+
 }
--- src/main/java/com/example/storeeverything/controllers/NoteController.java
+++ src/main/java/com/example/storeeverything/controllers/NoteController.java
@@ -4,13 +4,19 @@ import com.example.storeeverything.Repository.NoteRepository;
 import com.example.storeeverything.data.Category;
 import com.example.storeeverything.data.Note;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.GroupOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -57,4 +63,57 @@ public class NoteController {
         List<Note> notes = noteRepository.findByUserIdOrderByCreatedAtAsc(userId);
         return notes;
     }
+
+
+    // Endpoint to retrieve all notes for a user, sorted by title descending
+    @GetMapping("/user/{userId}/title/desc")
+    public List<Note> getNotesByTitleDesc(@PathVariable Integer userId) {
+        List<Note> notes = noteRepository.findByUserOrderByTitleDesc(userId);
+        return notes;
+    }
+
+    // Endpoint to retrieve all notes for a user, sorted by title ascending
+    @GetMapping("/user/{userId}/title/asc")
+    public List<Note> getNotesByTitleAsc(@PathVariable Integer userId) {
+        List<Note> notes = noteRepository.findByUserOrderByTitleAsc(userId);
+        return notes;
+    }
+
+    // Endpoint to retrieve all notes for a user, sorted by category alphabetically descending
+    @GetMapping("/user/{userId}/category/desc")
+    public List<Note> getNotesByCategoryDesc(@PathVariable Integer userId) {
+        List<Note> notes = noteRepository.findByUserOrderByCategoryDesc(userId);
+        return notes;
+    }
+
+    // Endpoint to retrieve all notes for a user, sorted by category alphabetically ascending
+    @GetMapping("/user/{userId}/category/asc")
+    public List<Note> getNotesByCategoryAsc(@PathVariable Integer userId) {
+        List<Note> notes = noteRepository.findByUserOrderByCategoryAsc(userId);
+        return notes;
+    }
+
+
+//    // Endpoint to retrieve all notes from all users, filtered by current date
+//    @GetMapping("/fromToday")
+//    public List<Note> getNotesFromToday() {
+//        LocalDate date = LocalDate.now();
+//        LocalDateTime dt = LocalDateTime.now();
+//        System.out.println(date);
+//        List<Note> notes = noteRepository.findByCreatedAt_Date(dt);
+//        System.out.println(notes);
+//        return notes;
+//    }
+
+//    @GetMapping("/mostPopularCategory")
+//    public  List<Note> getMPC(){
+//
+////        MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory);
+////
+////        GroupOperation groupOperation = Aggregation.group("category.name").count().as("count");
+////        Aggregation aggregation = Aggregation.newAggregation(groupOperation);
+////
+////        AggregationResults<Document> result = mongoTemplate.aggregate(aggregation, "notes", Document.class);
+////        List<Document> categoryCounts = result.getMappedResults();
+//    }
 }
