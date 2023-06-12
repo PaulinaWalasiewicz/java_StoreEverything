30b70ccd712cfb4f98e262b20180636a6a3d0504
alexrostkowska <aleksandra.rostkowskaa@gmail.com>
2023-06-12 17:30:39 +0200

    Fix data format

--- src/main/java/com/example/storeeverything/data/Note.java
+++ src/main/java/com/example/storeeverything/data/Note.java
@@ -1,9 +1,11 @@
 package com.example.storeeverything.data;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -16,13 +18,23 @@ public class Note {
     @Id
     private String id;
 
+    @JsonFormat(pattern="dd-MM-yyyy HH:mm")
     private LocalDateTime createdAt;
+
     private String title;
     private String content;
     @DBRef
     private Category category;
     @DBRef
     private  User user;
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 
     private String link;
 }
